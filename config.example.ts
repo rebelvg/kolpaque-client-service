86aefa2aabf5c80e14e5b6a6e6d4f6aa6635c88b
Rebel <johnhartigan@ya.ru>
2020-05-20 16:55:27 +0300

    change default port

--- config.example.ts
+++ config.example.ts
@@ -1,18 +1,18 @@
 export const server = {
-  port: 3000,
+  port: 8000,
   jwtSecret: 'jwt_secret',
 };
 
 export const twitch = {
   clientId: 'client_id',
   clientSecret: 'client_secret',
-  callbackUrl: 'http://localhost:3000/auth/twitch/callback',
+  callbackUrl: 'http://localhost:8000/auth/twitch/callback',
 };
 
 export const google = {
   clientId: 'client_id',
   clientSecret: 'client_secret',
-  callbackUrl: 'http://localhost:3000/auth/google/callback',
+  callbackUrl: 'http://localhost:8000/auth/google/callback',
 };
 
 export const youtube = {
